0940bba1587522048a432760058d1b72e00a0a29
André Timermann <andre@andregustavo.org>
2016-05-28 12:55:56 -0300

    Primeira versão estável/usuável do sindri-grid

--- index.js
+++ index.js
@@ -8,9 +8,13 @@
 const angular = require('angular');
 
 require('./node_modules/angular-ui-grid/ui-grid');
-require('./node_modules/angular-ui-grid/ui-grid.css');
 
-module.exports = angular.module("sindriGrid", ["ui.grid", "ui.grid.autoResize"])
+//require('./node_modules/angular-ui-grid/ui-grid.css');
+require('./css/customGrid.css');
+
+require('./lib/filter');
+
+module.exports = angular.module("sindriGrid", ["ui.grid", "ui.grid.autoResize",  "ui.grid.resizeColumns", "ui.grid.selection", "sindriFilters"])
 
     // CONFIGURAÇÃO DA DIRETIVA
     .directive('sindriGrid', require('./lib/directive'))
